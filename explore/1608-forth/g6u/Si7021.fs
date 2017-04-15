d187cabcfaadd5b8a17ddda5f6f19c14635a0e86
Joris Pragt <pragtich@github>
2017-04-15 12:58:39 +0200

    update Si7021 driver

--- explore/1608-forth/g6u/Si7021.fs
+++ explore/1608-forth/g6u/Si7021.fs
@@ -7,6 +7,23 @@
   0 65536 f/	( wasteful, but who cares )
   85 46 d-	( - 46.85 )
   ;
-: Si-RH     $40 i2c-addr $E5 >i2c 2 i2c-xfer i2c>h_inv Si7021-RH-conv ; \ Measure RH (wait)
-: Si-T     $40 i2c-addr $E3 >i2c 2 i2c-xfer i2c>h_inv Si7021-T-conv ;  \ Measure T (wait)
-: Si-lastT  $40 i2c-addr $E0 >i2c 2 i2c-xfer i2c>h_inv Si7021-T-conv ; \ Get T from last RH measurement
+: Si-RH      $40 i2c-addr $E5 >i2c 2 i2c-xfer i2c>h_inv Si7021-RH-conv ; \ Measure RH (wait)
+: Si-T       $40 i2c-addr $E3 >i2c 2 i2c-xfer i2c>h_inv Si7021-T-conv ;  \ Measure T (wait)
+: Si-lastT   $40 i2c-addr $E0 >i2c 2 i2c-xfer i2c>h_inv Si7021-T-conv ; \ Get T from last RH measurement
+: Si-serial1 ( -- u) \ Gets first 32 bits of serial
+  $40 i2c-addr $FA >i2c $0F >i2c
+  8 i2c-xfer
+  0
+  4 0 do
+    i2c> i2c> drop
+    swap 8 lshift or
+  loop ;
+
+: Si-serial2 ( -- u) \ Gets 2nd 32 bits of serial
+  $40 i2c-addr $FC >i2c $C9 >i2c
+  6 i2c-xfer
+  0
+  2 0 do
+    i2c>h_inv i2c> drop
+    swap 16 lshift or
+  loop ;
