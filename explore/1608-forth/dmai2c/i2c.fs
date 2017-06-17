66c6bd6ee6c319138cb153e9db6cdc0e0e1552b3
Joris Pragt <pragtich@gmail.com>
2017-06-17 18:46:36 +0200

    rewrite of i2c-xfer in progress

--- explore/1608-forth/dmai2c/i2c.fs
+++ explore/1608-forth/dmai2c/i2c.fs
@@ -168,12 +168,9 @@ $40005800 constant I2C2
   \ Does not wait for ADDR: i2x-xfer should do this
   i2c.txbuf i2c-bufsize init-ring
   i2c.rxbuf i2c-bufsize init-ring
+  shl i2c.addr !
 
-  i2c-start
-  shl dup i2c.addr !
   \ i2c-EV5
-  i2c-SR1-SB i2c-SR1-wait
-  i2c-DR!                   \ Sends address (write mode)
 ;
 
 : i2c-irq-tx-stop           \ irq handler for DMA transmission done
@@ -241,17 +238,11 @@ $40005800 constant I2C2
   else
     ['] i2c-irq-tx-rx   irq-dma1_6 !
   then
-  \ Wait for addr and clear it
-  i2c-SR1-ADDR i2c-SR1-AF or i2c-SR1-wait    \ Wait for ack or nak
-  I2C1-SR2 h@ drop                           \ clear ACK flag
-  i2c-SR1-AF i2c-SR1-flag?                   \ Put NAK on stack
-  i2c-SR1-AF i2c1-SR1 hbic!                  \ Clear the NAK flag
-
-  \ Stop if nothing to send   TODO this is wrong. When no tx, we need to go straight to rx somehow
 
   i2c.txbuf ring# 0= if
-    i2c-stop
-    begin 9 bit I2C1-CR1 hbit@ 0= until
+    \ i2c-stop
+    \ begin 9 bit I2C1-CR1 hbit@ 0= until
+    -1
   else
     \ Configure DMA
     %0011000010011010 DMA1-CCR6 !              \ 8 bit high prio mem to peripheral error&finish interrupt
@@ -263,7 +254,22 @@ $40005800 constant I2C2
     11 bit I2C1-CR2 hbis!                      \ DMAEN
     0 bit DMA1-CCR6 bis!                       \ DMAEN
 
+    i2c-start
+    i2c.addr @ i2c-DR!                        \ Sends address (write mode)
+    i2c-EV6                                    \ Wait for addr and clear
+    i2c-SR1-AF i2c-SR1-flag?                   \ Put NAK on stack
+    i2c-SR1-AF i2c1-SR1 hbic!                  \ Clear the NAK flag
+
   then
+
+
+  \ Wait for addr and clear it
+  \ i2c-SR1-ADDR i2c-SR1-AF or i2c-SR1-wait    \ Wait for ack or nak
+  \ I2C1-SR2 h@ drop                           \ clear ACK flag
+
+  \ Stop if nothing to send   TODO this is wrong. When no tx, we need to go straight to rx somehow
+  \ TODOO: case for rx# = 1, no DMA possible
+
 ;
 
 : >i2c  ( u -- ) \ Sends a byte over i2c. Use after i2c-addr
@@ -308,7 +314,7 @@ $40005800 constant I2C2
 led-init led-off
 i2c? cr
 i2c-init
-$40 i2c-addr 0 i2c-xfer .
+ $40 i2c-addr 0 i2c-xfer .
 i2c? cr
-$41 i2c-addr 0 i2c-xfer .
+ $41 i2c-addr 0 i2c-xfer .
 
