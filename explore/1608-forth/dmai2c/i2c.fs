323f5f9b0bec90f1e0acae15094e4005a89850aa
Joris Pragt <pragtich@github>
2017-07-29 13:57:45 +0200

    Prevent clobbering in case of subsequent teansmissions

--- explore/1608-forth/dmai2c/i2c.fs
+++ explore/1608-forth/dmai2c/i2c.fs
@@ -191,6 +191,9 @@ $40005800 constant I2C2
 : i2c-addr ( u --)
   \ Start a new transaction and send address in write mode
   \ Does not wait for ADDR: i2x-xfer should do this
+
+  begin pause i2c-busy? 0= until \ Wait until peripheral ready
+  
   i2c.txbuf i2c-bufsize init-ring
   i2c.rxbuf i2c-bufsize init-ring
   shl i2c.addr !
