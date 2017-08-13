6677cd2f228a9bd043d4706fd192125019cff7de
Joris Pragt <pragtich@gmail.com>
2017-08-13 18:25:52 +0200

    Initiate serial test case

--- explore/1608-forth/dmai2c-ser/core.fs
+++ explore/1608-forth/dmai2c-ser/core.fs
@@ -1,10 +1,8 @@
 \ core definitions
 
-\ <<<board>>>
+<<<board>>>
 compiletoflash
 ( core start: ) here hex.
-
-1 constant OLED.LARGE  \ display size: 0 = 128x32, 1 = 128x64 (default)
 
 include ../flib/i2c/ssd1306.fs
 include ../flib/mecrisp/graphics.fs
