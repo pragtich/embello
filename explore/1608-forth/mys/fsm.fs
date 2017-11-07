86bbca0760937ff69efe121fc303555aa6e19fba
Joris Pragt <pragtich@gmail.com>
2017-11-07 18:44:44 +0100

    Update to better version

--- explore/1608-forth/mys/fsm.fs
+++ explore/1608-forth/mys/fsm.fs
@@ -1,34 +1,27 @@
 \ Finite state machine
 \ By Julian Noble
 \ Edits for Mecrisp compatibility by Joris Pragt
-\ http://computer-programming-forum.com/22-forth/63a16bfb51edc2e9.htm
+\ https://web.archive.org/web/20011008085910/http://www.jfar.org:80/article001.html
+\ 
 \ Also see the excellent articles at http://galileo.phys.virginia.edu/classes/551.jvn.fall01/, including
 \ http://galileo.phys.virginia.edu/classes/551.jvn.fall01/fsm.html
 
 
 \ code to create state machines from tabular representations
 
-: ||   ' ,  ' ,  ;            \ add two xt's to data field
-: wide   0  ;                 \ aesthetic, initial state = 0
-: fsm:   ( width state --)    \ define fsm
-    CREATE  , ( state) ,      ( width in double-cells)  ;
+: WIDE   0 ;
 
-: ;fsm   DOES>                ( x col# adr -- x' )
+: FSM:   ( width 0 -- )
+      CREATE   ,  ,   ]
+      DOES>               ( col# adr -- )
+         DUP >R  2@  *  + ( -- col#+width*state )
+         2*  2+  CELLS    ( -- offset-to-action)
+         DUP >R           ( -- offset-to-action)
+         @ EXECUTE        ( ? )
+         R> CELL+         ( -- offset-to-update)
+         @ EXECUTE        ( -- state')
+         R> !   ;         \ update state
 
-         *  +                 ( x col#+width*state )
-         2*  2 +  CELLS       ( x relative offset )
-
-         DUP >R               ( x adr[action] )
-         EXECUTE              ( x' )
-         R> CELL+             ( x' adr[update] )
-         EXECUTE              ( x' state')
-         R> !   ;             ( x' )  \ update state
-
-\ set fsm's state, as in:  0 >state fsm-name
-\ : >state   POSTPONE defines  ; IMMEDIATE   ( state "fsm-name" --)
-
-\ : state: ( "fsm-name" -- state) \ get fsm's state
-\     'dfa                  \ get dfa
 
 0 CONSTANT >0   3 CONSTANT >3   6 CONSTANT >6    \ these indicate state
 1 CONSTANT >1   4 CONSTANT >4   7 CONSTANT >7    \ transitions in tabular
@@ -55,10 +48,9 @@
 4 WIDE FSM: <Fixed.Pt;#>
 \ input:  |  other?  |  num?   |  minus?  |   dp?     |
 \ state:  ---------------------------------------------
-   ( 0 )    || DROP >0  ||  EMIT >1 ||  EMIT >1  ||   EMIT >2
-   ( 1 )    || DROP >1  ||  EMIT >1 ||  DROP >1  ||   EMIT >2 
-   ( 2 )    || DROP >2  ||  EMIT >2 ||  DROP >2  ||   DROP >2
-;fsm
+   ( 0 )     DROP >0    EMIT >1   EMIT >1     EMIT >2
+   ( 1 )     DROP >1    EMIT >1   DROP >1     EMIT >2
+   ( 2 )     DROP >2    EMIT >2   DROP >2     DROP >2 ;
 
 : Getafix   0  ' <Fixed.Pt;#> !
             BEGIN   KEY   DUP   13 <>      WHILE
